5c772920ea9bd5e163f27ac2112bc37901001814
尹越 <yinxiyue1995@outlook.com>
2026-03-16 15:35:33 +0800

    feat: init MAUI

---
+++ client/QwertyClient/QwertyClient/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace QwertyClient
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ client/QwertyClient/QwertyClient/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace QwertyClient
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
