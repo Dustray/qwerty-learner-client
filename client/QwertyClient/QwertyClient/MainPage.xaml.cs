0adb08f9cebb1dc97143bb295954988d5e11e811
尹越 <yinxiyue1995@outlook.com>
2026-03-16 19:52:38 +0800

    Revert "fix: error"
    This reverts commit 327cb5c4df1c0e9616da6b358ca32c7e47be085a.

--- client/QwertyClient/QwertyClient/MainPage.xaml.cs
+++ client/QwertyClient/QwertyClient/MainPage.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Maui.Controls;
-using System;
 
 namespace QwertyClient
 {
@@ -8,16 +7,6 @@ namespace QwertyClient
         public MainPage()
         {
             InitializeComponent();
-            ConfigureWebView();
-        }
-
-        private void ConfigureWebView()
-        {
-            // 配置WebView的基本设置
-            webView.Navigating += (sender, e) =>
-            {
-                // 可以在这里添加导航拦截逻辑
-            };
         }
     }
 }
